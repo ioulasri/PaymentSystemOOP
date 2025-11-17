314a377ea4550900a4b7895a2a542f43a9db8405
imad <imad.oulasri@iu-study.org>
2025-11-17 20:25:26 +0100

    feat: Add ProjectError exception class with appropriate fields
    ValidtionError inherits from ProjectError
    
    PaymentError inherits from ProjectError

--- src/payment/exceptions.py
+++ src/payment/exceptions.py
@@ -1,8 +1,20 @@
 
-class ValidationError(Exception):
+class ProjectError(Exception):
 	"""Raised when input validation fails."""
 
 	def __init__(self, message, field=None):
 		self.message = message
 		self.field = field
 		super().__init__(f"{message}" + (f" (Field): {field}") if field else "")
+
+class ValidationError(ProjectError):
+	"""Raised when input validation fails."""
+
+	def __init__(self, message, field=None):
+		super().__init__(message, field)
+		
+class PaymentError(Exception):
+	"""Raised when payment fails."""
+
+	def __init__(self, message, field=None):
+		super().__init__(message, field)
