4875ede489a566aaf5f7ca8acb442915751d9e75
imad <imad.oulasri@iu-study.org>
2025-11-17 20:36:06 +0100

    Fix: Removed attributes until needed

--- src/payment/payment_strategy.py
+++ src/payment/payment_strategy.py
@@ -1,12 +1,6 @@
 from abc import ABC, abstractmethod
 
 class PaymentStrategy(ABC):
-	def __init__(self):
-		super().__init__()
-		self._timestamp
-		self._transaction_id
-		self.status
-
 	@abstractmethod
 	def validate(self) -> bool:
 		pass
