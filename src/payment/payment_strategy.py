e4439abb613eeb697bf91051f4690f4a0a1cdbd8
imad <imad.oulasri@iu-study.org>
2025-11-18 15:59:38 +0100

    add: Establish common attributes: status, transaction_id and timestamp

--- src/payment/payment_strategy.py
+++ src/payment/payment_strategy.py
@@ -1,6 +1,14 @@
 from abc import ABC, abstractmethod
+from datetime import date
+from uuid import uuid4
 
 class PaymentStrategy(ABC):
+	def __init__(self):
+		super().__init__()
+		self.timestamp: date = date.today()
+		self.status: str
+		self.transaction_id: str = f"TX-"+{uuid4}
+
 	@abstractmethod
 	def validate(self) -> bool:
 		pass
